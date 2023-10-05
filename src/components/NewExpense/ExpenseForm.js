de62d0aa1ca94181e3b5a936387920f8ae0ce1cc
Ashish Parashar <200104016@hbtu.ac.in>
2023-10-05 20:14:04 +0530

    Two-way binding Done

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Css/ExpenseForm.css';
 const ExpenseForm = () => {
+    const [userInput,setUserInput]=useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate:''
+
+    });
+//     const [enteredTitle, setEnteredTitle] = useState('');
+//     const [enteredAmount, setEnteredAmount] = useState('');
+//     const [enteredDate, setEnteredDate] = useState('');
+//   //we can have more than one state per component
+//   //we can even use one state per component
+    const titleChangeHandler = (e) => {
+       setUserInput((prevState)=>{
+        return {
+            ...prevState,
+        enteredTitle: e.target.value,
+        }
+       });
+    }
+    //this is much better i suppose
+    const amountChangeHandler = (e) => {
+        setUserInput((prevState)=>{
+            return{
+                ...prevState,
+                enteredAmount: e.target.value,
+            }
+            
+           })
+       
+    }
+    const dateChangeHandler = (e) => {
+        setUserInput((prevState)=>{
+            return{
+                ...prevState,
+                enteredDate: e.target.value,
+            }
+           })
+    }
+    const submitHandler=(e)=>{
+       e.preventDefault();
+       const expenseData={
+        title:userInput.enteredTitle,
+        amount:userInput.enteredAmount,
+        date:new Date(userInput.enteredDate)
+       }
+       setUserInput((prevState)=>{
+        return{
+            enteredDate: '',
+            enteredAmount:'',
+            enteredTitle:''
+        }
+       })
+      
+       console.log(expenseData);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type='text' />
+                        <input type='text' onChange={titleChangeHandler} value={userInput.enteredTitle}/>
+                        {/* we  can use onChange event for all input type*/}
                     </div>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type='number' min="0.01" />
+                        <input type='number' min="0.01" onChange={amountChangeHandler} value={userInput.enteredAmount}/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Date</label>
-                        <input type='date' min="2019-01-01" max="2023-12-31" />
+                        <input type='date' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} value={userInput.enteredDate}/>
                     </div>
                 </div>
                 <div className='new-expense__actions'>
-                    <button type='submit'>Add expense</button>
+                    <button type='submit' >Add expense</button>
                 </div>
             </form>
         </div>
